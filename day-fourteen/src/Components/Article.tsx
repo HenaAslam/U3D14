a552ce8db41b6fd4cde359e6c45dd80ae2a64827
ajeer333 <ajeer333@gmail.com>
2023-02-16 18:07:31 +0100

    commit

--- day-fourteen/src/Components/Article.tsx
+++ day-fourteen/src/Components/Article.tsx
@@ -1,91 +1,56 @@
-import { useEffect, useState } from "react"
-import { Container, Row,Col,Spinner, Alert, } from "react-bootstrap"
-import { IArticle } from "../interfaces/IArticle"
+import { useEffect, useState } from "react";
+import { Container, Row, Col, Spinner, Alert } from "react-bootstrap";
+import { IArticle } from "../interfaces/IArticle";
 
-import SingleArticle from "./SingleArticle"
+import SingleArticle from "./SingleArticle";
 
+const Article = () => {
+  const [news, setNews] = useState<IArticle[]>([]);
+  const [isLoading, setiIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
+  const fetchNews = async () => {
+    try {
+      setiIsLoading(true);
 
-const Article=()=>{
-
-    const[news,setNews]=useState<IArticle[]>([])
-    const [isLoading,setiIsLoading]=useState(false)
-    const[isError, setIsError] = useState(false)
-
-
-   
-    
-
-    const fetchNews=async()=>{
-
-        try{
-            setiIsLoading(true)
-            
-            let res= await fetch("https://api.spaceflightnewsapi.net/v3/articles")
-            if(res.ok){
-                let newsfromapi=await res.json()
-                console.log(newsfromapi)
-                setNews(newsfromapi)
-                setiIsLoading(false)
-            }
-            else{
-                setIsError(true)
-                setiIsLoading(false)
-            }
-
-        }
-        catch(error){
-            setIsError(true)
-            setiIsLoading(false)
-        }
+      let res = await fetch("https://api.spaceflightnewsapi.net/v3/articles");
+      if (res.ok) {
+        let newsfromapi = await res.json();
+        console.log(newsfromapi);
+        setNews(newsfromapi);
+        setiIsLoading(false);
+      } else {
+        setIsError(true);
+        setiIsLoading(false);
+      }
+    } catch (error) {
+      setIsError(true);
+      setiIsLoading(false);
     }
+  };
 
+  useEffect(() => {
+    fetchNews();
+  }, []);
 
-    useEffect(()=>{
-        fetchNews()
-    },[])
+  return (
+    <Container fluid>
+      {isLoading && <Spinner animation="grow" variant="danger" />}
+      {isError && <Alert variant="danger">Something's wrong..</Alert>}
 
-    return(
-       <Container >
-     
-        {isLoading && <Spinner animation="grow" variant="danger" />}
-        {isError && <Alert variant="danger">Something's wrong..</Alert>}
-
-       
-            <Row className="justify-content-center mt-5 ">
-                <Col>
-            
-
-
-
-              
-  <h1 >SPACEFLICHT NEWS</h1>
-
-
-
-
-
-                </Col>
-            </Row>
-            <Row className="justify-content-center mt-5 text-dark">
-                
-           
-                {news.map(n=>(
-                  
-                  <Col  key={n.id} xs={8} md={4} lg={3} className="text-dark mb-3"  >
-<SingleArticle art={n} />
-
-
-
-</Col>     
-
-                )
-                    )}
-                
-                
-            </Row>
-       </Container>
-
-    )
-}
-export default Article
+      <Row className="justify-content-center mt-5 ">
+        <Col>
+          <h1>SPACEFLIGHT NEWS</h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-center mt-5 text-dark">
+        {news.map((n) => (
+          <Col key={n.id} xs={8} md={4} lg={3} className="text-dark mb-3">
+            <SingleArticle art={n} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+export default Article;
--- day-fourteen/src/Components/Details.tsx
+++ day-fourteen/src/Components/Details.tsx
@@ -1,70 +1,57 @@
-import { useEffect, useState } from "react"
-import {useParams} from "react-router-dom"
-import { IArticle } from "../interfaces/IArticle"
-import {Spinner} from "react-bootstrap"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { IArticle } from "../interfaces/IArticle";
+import { Spinner, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import SingleDetails from "./SingleDetails";
 
+const Details = () => {
+  const [single, setSingle] = useState<null | IArticle>(null);
+  const [isLoading, setiIsLoading] = useState(false);
+  const params = useParams();
+  const navigate = useNavigate();
 
- import SingleDetails from "./SingleDetails"
+  const fetchSingleArticle = async () => {
+    try {
+      setiIsLoading(true);
+      let res = await fetch(
+        "https://api.spaceflightnewsapi.net/v3/articles/" + params.id
+      );
+      if (res.ok) {
+        let data = await res.json();
 
-const Details=()=>{
-
-
-
-    const[single,setSingle]=useState <null | IArticle> (null)
-    const [isLoading,setiIsLoading]=useState(false)
-    const params=useParams()
- 
-    
-
-
-    const fetchSingleArticle=async()=>{
-        try{
-            setiIsLoading(true)
-            let res=await fetch("https://api.spaceflightnewsapi.net/v3/articles/"+params.id)
-            if(res.ok){
-                let data= await res.json()
-                
-                setSingle(data)
-                setiIsLoading(false)
-               
-            }
-            else{
-                console.log("error")
-                setiIsLoading(false)
-            }
-
-        }
-        catch(error){
-            console.log(error)
-            setiIsLoading(false)
-        }
-
-      
-      
-
+        setSingle(data);
+        setiIsLoading(false);
+      } else {
+        console.log("error");
+        setiIsLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      setiIsLoading(false);
     }
+  };
 
-    useEffect(()=>{
-        fetchSingleArticle()
+  useEffect(() => {
+    fetchSingleArticle();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[])
+  }, []);
 
-   
-
-
-    return(
-        <>
-       
-
-       {isLoading && <Spinner animation="grow" variant="danger" />}
-        {single &&  <SingleDetails art={single}/>}
-        {console.log(single)}
-       
-       
-        </>
-
-    )
-
-}
-export default Details
-
+  return (
+    <>
+      <Button
+        variant="success"
+        className="mt-5"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Back to home
+      </Button>
+      {isLoading && <Spinner animation="grow" variant="danger" />}
+      {single && <SingleDetails art={single} />}
+      {console.log(single)}
+    </>
+  );
+};
+export default Details;
